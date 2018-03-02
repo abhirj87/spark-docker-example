8b269a21e5742cc150b305f2b634c5f9250849ee
aiyenger <abhiram.r.iyenger@aexp.com>
2018-03-01 20:12:29 -0700

    logic to add the validator

--- src/main/resources/start_job.sh
+++ src/main/resources/start_job.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
+#
+# Spark submit command
+#
 spark-submit --class org.log_analyser.AccessLogProcessor --master spark://spark:7077 top_n_processor-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
 input \
 output_data \
 spark://spark:7077 \
 5 \
 3
+
+#
+#Following peice of code is to validate the output
+#
+#
+file=NASA_access_log_Jul95
+for ln in $(tail -n 3 -q output_data/*)
+do
+dt=$(echo $ln | cut -d ',' -f1)
+ip=$(echo $ln | cut -d ',' -f2)
+count=$(echo $ln | cut -d ',' -f3)
+echo $ip $dt $count  all from the output
+var=$(echo $(cat $file | grep $dt | grep $ip | wc -l))
+echo $var == from the input
+if [ $var -eq $count ]; then
+    echo "Matched!!!"
+else
+    echo "Validation failed!!" $var $count
+    echo "Validation failed!!"
+    echo "Validation failed!!"
+    echo "Validation failed!!"
+    echo "Validation failed!!"
+    echo "Validation failed!!"
+    echo "Validation failed!!"
+    echo "Validation failed!!"
+    echo "Validation failed!!"
+fi
+done
